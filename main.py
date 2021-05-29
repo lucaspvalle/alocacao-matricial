934032e9d21635628721d95dbd363d1bedcc1f32
Lucas Valle <lucaspvalle@gmail.com>
2021-05-29 17:36:20 -0300

    Melhorias no código

--- main.py
+++ main.py
@@ -14,11 +14,10 @@ df = (df
       .merge(limites, left_on=["Time"], right_index=True)
       .fillna(0))
 
-# Solver
-solver = pywraplp.Solver.CreateSolver("Alocação de Membros", "CBC")
+df["Fixo"] = df["Fixo"] != 0  # Convertendo em booleano para restringir domínio
 
-# Convertendo em booleano para restringir domínio
-df["Fixo"] = df["Fixo"] != 0
+# Modelo
+solver = pywraplp.Solver.CreateSolver("Alocação de Membros", "CBC")
 
 # Variável de decisão
 df["X"] = df.apply(lambda r: solver.BoolVar(f"X_{r.Nome}_{r.Time}") if not r["Fixo"] else 1, axis=1)
@@ -38,15 +37,14 @@ solver.Minimize(solver.Sum(df.query('~ Fixo').apply(lambda r: r['Interesse'] * r
  .groupby('Nome')
  .apply(lambda r: solver.Add(solver.Sum(r['X']) == 1, f"aloca_projetos_{r.iloc[0].Nome}")))
 
-# O time (área ou projeto) possui um número máximo de membros
-(df
- .groupby("Time")
- .apply(lambda r: solver.Add(solver.Sum(r['X']) <= r.iloc[0]['Max'], f"limite_max_{r.iloc[0].Time}")))
 
-# O time (área ou projeto) possui um número mínimo de membros
-(df
- .groupby("Time")
- .apply(lambda r: solver.Add(solver.Sum(r['X']) >= r.iloc[0]['Min'], f"limite_min_{r.iloc[0].Time}")))
+def limita_membros_no_time(r):
+    solver.Add(solver.Sum(r['X']) <= r.iloc[0]['Max'], f"limite_max_{r.iloc[0].Time}")  # Limite máximo de membros
+    solver.Add(solver.Sum(r['X']) >= r.iloc[0]['Min'], f"limite_min_{r.iloc[0].Time}")  # Limite mínimo de membros
+
+
+# O time (área ou projeto) possui limites inferiores e superiores de membros
+df.groupby("Time").apply(limita_membros_no_time)
 
 status = solver.Solve()
 
